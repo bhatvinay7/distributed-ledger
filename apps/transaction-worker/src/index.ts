395a535fc118eec1bfa72b52be11cd95ca1d08b0
vinay bhat <bhatvinay74@gmail.com>
2025-12-09 21:45:32 +0530

    update the index.ts file

---
+++ apps/transaction-worker/src/index.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import  {setupSubscriptionGroup,startWorker} from './eventStore-worker.js'
+const PORT=3005
+try{
+(async () => {
+  await setupSubscriptionGroup();
+  await startWorker();
+})();
+}
+catch(error:any){
+  console.log(error.message)
+}
+
+const app = express();
+  app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Consumer collector running on port ${PORT}`);
+  });
+// }
