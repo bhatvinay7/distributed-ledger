d065acd5fd4e803e958f912a1ecc22666a3a4677
vinay bhat <bhatvinay74@gmail.com>
2025-12-10 16:17:09 +0530

    update the database with actual ledger data,journal and audit logs

---
+++ apps/projection-worker/src/processTransaction.ts
@@ -0,0 +1,113 @@
+import { prisma,Prisma } from 'prisma'
+export async function processTransaction(event: any) {
+    const {
+        transactionId,
+        senderId,
+        receiverId,
+        debit,
+        credit,
+        debitType,
+        creditType,
+        note,
+        amount,
+        createdAt
+    } = event;
+
+    return prisma.$transaction(async (tx) => {
+        try {
+
+            const [sender, receiver] = await Promise.all([
+                tx.account.findUnique({ where: { id: senderId } }),
+                tx.account.findUnique({ where: { id: receiverId } }),
+            ]);
+
+            if (!sender) throw new Error("Sender not found");
+            if (!receiver) throw new Error("Receiver not found");
+
+            const debitDecimal = new Prisma.Decimal(parseFloat(debit));
+            const creditDecimal = new Prisma.Decimal(parseFloat(credit));
+
+            const senderNewBalance = sender.balance.minus(debitDecimal);
+            const receiverNewBalance = receiver.balance.plus(creditDecimal);
+
+            const senderEntry = await tx.ledgerEntry.create({
+                data: {
+                    transactionId,
+                    accountId: senderId,
+                    debit: debitDecimal,
+                    credit: new Prisma.Decimal(0),
+                    debitType:"Online",
+                    creditType: "None",
+                    balance: senderNewBalance,
+                    note:`${debitDecimal} debited from your account`,
+                    createdAt: createdAt ? new Date(createdAt) : new Date(),
+                },
+            });
+
+            const receiverEntry = await tx.ledgerEntry.create({
+                data: {
+                    transactionId,
+                    accountId: receiverId,
+                    debit: new Prisma.Decimal(0),
+                    credit: creditDecimal,
+                    debitType: "None",
+                    creditType:"Online",
+                    balance: receiverNewBalance,
+                    note:`${creditDecimal} is created to your account`,
+                    createdAt: createdAt ? new Date(createdAt) : new Date(),
+                },
+            });
+
+            await tx.journal.create({
+                data: {
+                    transactionId,
+                    eventType: "TRANSACTION_CREATED",
+                    timestamp:  createdAt ? createdAt: new Date(),
+                    data: {
+                        summary: `Transfer of ₹${amount} from ${senderId} to ${receiverId}`,
+                        entries: [
+                            {
+                                type: "DEBIT",
+                                accountId: senderId,
+                                message: `Account ${senderId} was debited ₹${amount}`,
+                                amount: amount
+                            },
+                            {
+                                type: "CREDIT",
+                                accountId: receiverId,
+                                message: `Account ${receiverId} was credited ₹${amount}`,
+                                amount: amount
+                            }
+                        ]
+                    }
+                },
+            });
+
+            await tx.auditLog.createMany({
+                data: [
+                    {
+                        userId: senderId,
+                        action: "DEBIT",
+                        entity: "Account",
+                        entityId: senderId,
+                        previousValue: { balance: sender.balance.toString() },
+                        newValue: { balance: senderNewBalance.toString() },
+                    },
+                    {
+                        userId: receiverId,
+                        action: "CREDIT",
+                        entity: "Account",
+                        entityId: receiverId,
+                        previousValue: { balance: receiver.balance.toString() },
+                        newValue: { balance: receiverNewBalance.toString() },
+                    },
+                ],
+            });
+
+            return { senderEntry, receiverEntry };
+        }
+        catch (error: any) {
+            throw error
+        }
+    });
+}
